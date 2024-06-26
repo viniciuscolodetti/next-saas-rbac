e8d9dd6830c3ff35623c800b8090c99b407c488f
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2024-06-26 17:13:45 -0300

    finalizar permissões da aplicação

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,12 @@
-import { defineAbilityFor } from '@saas/auth'
+import { defineAbilityFor, projectSchema } from '@saas/auth'
 
-const ability = defineAbilityFor({ role: 'MEMBER' })
+const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
+const project = projectSchema.parse({
+  id: 'project-id',
+  ownerId: 'other-user-id',
+})
 
-const userCannotDeleteOthersUsers = ability.cannot('delete', 'User')
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteOtherUsers)
-console.log(userCannotDeleteOthersUsers)
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
+console.log(ability.can('delete', project))
