e2cc16fc308a77757efedadd4c1664cfe338b1ea
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2024-07-01 14:29:13 -0300

    rotas de criação e detalhe de um convite

---
+++ apps/api/src/http/routes/invites/get-invite.ts
@@ -0,0 +1,76 @@
+import { roleSchema } from '@saas/auth'
+import type { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
+
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function getInvite(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    'invite/:inviteId',
+    {
+      schema: {
+        tags: ['invites'],
+        summary: 'Get a invite',
+        params: z.object({
+          inviteId: z.string().uuid(),
+        }),
+        response: {
+          200: z.object({
+            invite: z.object({
+              id: z.string().uuid(),
+              createdAt: z.date(),
+              role: roleSchema,
+              email: z.string(),
+              organization: z.object({
+                name: z.string(),
+                avatarUrl: z.string().url().nullable(),
+              }),
+              author: z
+                .object({
+                  id: z.string().uuid(),
+                  name: z.string().nullable(),
+                  avatarUrl: z.string().url().nullable(),
+                })
+                .nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request) => {
+      const { inviteId } = request.params
+
+      const invite = await prisma.invite.findUnique({
+        select: {
+          id: true,
+          email: true,
+          role: true,
+          createdAt: true,
+          author: {
+            select: {
+              id: true,
+              name: true,
+              avatarUrl: true,
+            },
+          },
+          organization: {
+            select: {
+              name: true,
+              avatarUrl: true,
+            },
+          },
+        },
+        where: { id: inviteId },
+      })
+
+      if (!invite) {
+        throw new BadRequestError('Invite not found')
+      }
+
+      return { invite }
+    },
+  )
+}
