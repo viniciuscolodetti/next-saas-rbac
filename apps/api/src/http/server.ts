f44f0d085405fa9eb5b444ec160dc3117af201a9
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2024-07-01 11:17:24 -0300

    rota de atualização de organização

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -22,6 +22,7 @@ import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
 import { getOrganization } from './routes/orgs/get-organization'
 import { getOrganizations } from './routes/orgs/get-organizations'
+import { updateOrganization } from './routes/orgs/update-organization'
 import { verifyDomain } from './routes/orgs/verify-domain'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -74,6 +75,7 @@ app.register(verifyDomain)
 app.register(getMembership)
 app.register(getOrganization)
 app.register(getOrganizations)
+app.register(updateOrganization)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
