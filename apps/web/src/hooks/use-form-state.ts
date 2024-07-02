87df82421a2b12c3a808e0ed2792911d1ff149f5
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2024-07-02 11:03:48 -0300

    realizar login com email e senha

--- apps/web/src/hooks/use-form-state.ts
+++ apps/web/src/hooks/use-form-state.ts
@@ -8,6 +8,7 @@ interface FormState {
 
 export function useFormState(
   action: (data: FormData) => Promise<FormState>,
+  onSuccess?: () => Promise<void> | void,
   initialState?: FormState,
 ) {
   const [isPending, startTransition] = useTransition()
@@ -29,6 +30,10 @@ export function useFormState(
 
     startTransition(async () => {
       const state = await action(data)
+
+      if (state.success === true && onSuccess) {
+        await onSuccess()
+      }
 
       setFormState(state)
     })
