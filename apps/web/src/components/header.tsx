4e9360551172b2f004c2b56c7a59fd7de3b7e59e
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2024-07-02 15:50:36 -0300

    componente header

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,3 +1,23 @@
+import Image from 'next/image'
+
+import rocketseatIcon from '@/assets/rocketseat-icon.svg'
+
+import { ProfileButton } from './profile-button'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <div className="mx-auto flex max-w-[1200px] items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image
+          src={rocketseatIcon}
+          className="size-6 dark:invert"
+          alt="rocketseat"
+        />
+      </div>
+
+      <div className="flex items-center gap-4">
+        <ProfileButton />
+      </div>
+    </div>
+  )
 }
