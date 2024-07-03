9d0bb2455e97b44b159d8cd98b1fe6ddd7d78151
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2024-07-03 10:48:13 -0300

    configuração de parallel routes e interception routes no next.js

--- apps/web/src/app/(app)/page.tsx
+++ apps/web/src/app/(app)/page.tsx
@@ -1,7 +1,12 @@
+import { Header } from '@/components/header'
+
 export default async function Home() {
   return (
-    <div className="py-4">
-      <h1>Select a organization</h1>
+    <div className="space-y-4 py-4">
+      <Header />
+      <main className="mx-auto w-full max-w-[1200px] space-y-4">
+        <p className="text-sm text-muted-foreground">Select an organization</p>
+      </main>
     </div>
   )
 }
